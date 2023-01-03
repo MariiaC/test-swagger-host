9307dd756b152ff06a22754d0dbf020bbd0e0520
MariiaC <mariiachvorun@gmail.com>
2023-01-03 11:45:56 +0200

    init

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const logger = require("morgan");
+const  cors = require ('cors');
+const dotenv = require ('dotenv');
+
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require ('./swagger.json') ;
+
+// const usersRouter = require ('./routes/users.js');
+// const categoriesRouter = require ('./routes/categories.js');
+// const transactionsRouter = require ('./routes/transactions.js');
+
+dotenv.config();
+
+const app = express();
+
+const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
+
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
+
+app.use('/api-docs',
+    swaggerUi.serve,
+    swaggerUi.setup(swaggerDocument));
+
+// app.use('/api/users', usersRouter);
+// app.use('/api/categories', categoriesRouter);
+// app.use('/api/transactions', transactionsRouter);
+
+app.use((req, res) => {
+    res.status(404).json({ message: 'Not found' });
+});
+
+app.use((err, req, res, next) => {
+    const { message = 'Server error', status = 500 } = err;
+    res.status(status).json({ message: message });
+});
+
+// export default app;
+module.exports = app;
